f8c944ab227fceb1fe85d1192c10a01e2764a1b1
TechLionDev <jr.isamuel08.dev@gmail.com>
2022-12-05 19:38:34 -0500

    Enhance Phase 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
-import java.text.BreakIterator;
 import java.util.Scanner;
+@SuppressWarnings({"RedundantIfStatement", "ConstantValue"})
 public class Main {
-    public static Scanner TerminalInput = new Scanner(System.in);
+    public static final Scanner TerminalInput = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("|--------------------------------|");
         System.out.println("| Welcome to Leap Year Detector! |");
@@ -21,41 +21,29 @@ public class Main {
     }
 
     private static boolean checkYear(int year) {
-        if (year < 1582){
+        if (year < 1582) {
             System.out.println("Leap Years Did Not Exist Before 1582!");
             return false;
         }
         boolean isDivisibleBy4 = divisibleBy4(year);
         boolean isDivisibleBy100 = divisibleBy100(year);
         boolean isDivisibleBy400 = divisibleBy400(year);
-        if(isDivisibleBy4 && !isDivisibleBy100){
+        if (isDivisibleBy4 && !isDivisibleBy100) {
             return true;
-        } else if(isDivisibleBy4 && isDivisibleBy100 && isDivisibleBy400) {
+        } else if (isDivisibleBy4 && isDivisibleBy100 && isDivisibleBy400) {
             return true;
-        } else{
+        }else{
             return false;
         }
     }
     private static boolean divisibleBy4(int year) {
-        if (year%4 == 0){
-            return true;
-        }else{
-            return false;
-        }
+        return year % 4 == 0;
     }
     private static boolean divisibleBy100(int year) {
-        if (year%100 == 0){
-            return true;
-        }else{
-            return false;
-        }
+        return year % 100 == 0;
     }
     private static boolean divisibleBy400(int year) {
-        if (year%400 == 0){
-            return true;
-        }else{
-            return false;
-        }
+        return year % 400 == 0;
     }
 
 }
